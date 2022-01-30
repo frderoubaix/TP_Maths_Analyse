2e858c20216d77f321b9bd42e4934735e0d86008
François Deroubaix <francois.deroubaix@outlook.fr>
2022-01-30 15:10:28 +0100

    tp2

--- tp2.py
+++ tp2.py
@@ -1,22 +1,32 @@
 import numpy as np
 import scipy.linalg as la
+from math import *
 
-A = np.array([[1, 1, 1],
-              [-1, 1, 1],
-              [1, 2, 3]])
+A = np.array([
+                [-sin(pi/6), 0, -sin(pi/4), 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, -1, -cos(pi/4), 0, cos(pi/4), 1, 0, 0, 0, 0, 0],
+                [0, 0, sin(pi/4), 0, sin(pi/4), 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, -1, -cos(pi/4), 0, cos(pi/4), 1, 0, 0, 0],
+                [0, 0, 0, 0, -sin(pi/4), 0, -sin(pi/4), 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, -1, -cos(pi/4), 0, cos(pi/4), 1, 0],
+                [0, 0, 0, 0, 0, 0, sin(pi/6), 0, sin(pi/4), 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, -1, -cos(pi/4), 0, cos(pi/6)],
+                [0, 0, 0, 0, 0, 0, 0, 0, -sin(pi/4), 0, -sin(pi/6)],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -cos(pi/6)],
+                [-cos(pi/6), 0, cos(pi/4), 1, 0, 0, 0, 0, 0, 0, 0]
+              ])
 
 
 def factoLU(a):
     P, L, U = la.lu(a)
-    return np.concatenate((L, U), axis=1)
+    return np.concatenate((L, U, P), axis=1)
 
 
 def drLU(C, b):
-    lu = np.hsplit(C, 2)
-    lu = lu[1] + lu[0]
-    print(lu)
-    # return la.lu_solve(lu, b)
-
+    lu = np.hsplit(C, 3)
+    final_lu = np.add(lu[1], lu[0]) - lu[2]
+    piv = la.lu_factor(A)
+    return la.lu_solve((final_lu, piv[1]), b)
 
 
 # test facto LU
@@ -24,9 +34,19 @@ LU = factoLU(A)
 print(LU)
 
 # test drLU
-b = np.array([[1],
-              [3],
-              [6]])
-lu, piv = la.lu_factor(A)
-drLU(LU, b)
-print(lu)
+b = np.array([
+                [0],
+                [0],
+                [10000],
+                [0],
+                [0],
+                [0],
+                [20000],
+                [0],
+                [0],
+                [0],
+                [0]
+])
+result = drLU(LU, b)
+
+print(result)
